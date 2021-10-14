5293453e9ef5fdbf4e413c93e2107b3f7f1d931a
Alexander Aeschbacher <narwal27@protonmail.com>
2021-10-14 21:43:40 +0200

    initial implement, check for existing username

--- ese2021-project-scaffolding/scaffolding/backend/src/services/user.service.ts
+++ ese2021-project-scaffolding/scaffolding/backend/src/services/user.service.ts
@@ -8,7 +8,19 @@ export class UserService {
     public register(user: UserAttributes): Promise<UserAttributes> {
         const saltRounds = 12;
         user.password = bcrypt.hashSync(user.password, saltRounds); // hashes the password, never store passwords as plaintext
-        return User.create(user).then(inserted => Promise.resolve(inserted)).catch(err => Promise.reject(err));
+        return User.findOne({
+            where: {
+                userName: user.userName
+            }
+        })
+        .then(foundUser => {
+          if (foundUser == null) {
+            return User.create(user).then(inserted => Promise.resolve(inserted));
+        } else {
+          return Promise.reject({ message: 'account already exists'});
+        }
+      })
+      .catch(err => Promise.reject({ message: err}));
     }
 
     public login(loginRequestee: LoginRequest): Promise<User | LoginResponse> {
