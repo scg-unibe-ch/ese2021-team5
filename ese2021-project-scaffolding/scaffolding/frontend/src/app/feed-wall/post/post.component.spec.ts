007aa238af5ee8d4a2157d36d6995e6c0b37f9be
Andri Paulin <andpau200@gmail.com>
2021-11-25 09:31:46 +0100

    Added test for post.component.ts#ngOnInit()

--- ese2021-project-scaffolding/scaffolding/frontend/src/app/feed-wall/post/post.component.spec.ts
+++ ese2021-project-scaffolding/scaffolding/frontend/src/app/feed-wall/post/post.component.spec.ts
@@ -1,27 +1,52 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PostComponent } from './post.component';
+import {FeedWallComponent} from "../feed-wall.component";
+import {HttpClientTestingModule, HttpTestingController} from "@angular/common/http/testing";
+import {environment} from "../../../environments/environment";
 
 describe('PostComponent', () => {
   let component: PostComponent;
-  let fixture: ComponentFixture<PostComponent>;
+  let httpMock: HttpTestingController;
+
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ PostComponent ]
+      declarations: [ PostComponent ],
+      imports: [HttpClientTestingModule],
+      providers: [PostComponent]
     })
-    .compileComponents();
+      .compileComponents();
+    httpMock = TestBed.inject(HttpTestingController);
+    component = TestBed.inject(PostComponent);
   });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(PostComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+  afterEach(() => {
+    httpMock.verify(); //Verifies that no requests are outstanding.
   });
 
-  /*
+
   it('should create', () => {
     expect(component).toBeTruthy();
   });
-   */
+
+  it('should get image fileName', (done: DoneFn) => {
+
+    const mockResponse = {
+      fileName: "mockFilename",
+    }
+    component.post.pictureId = 1;
+
+    component.ngOnInit();
+
+    const mockRequest = httpMock.expectOne(
+      environment.endpointURL + 'post/' + component.post.pictureId + "/image"
+    )
+    mockRequest.flush(mockResponse);
+    done();
+
+    expect(component.post.pictureLink).toEqual(environment.endpointURL + "public/mockFilename");
+
+  })
+
 });
