bb0538d5f58cb02ab1f7c04d6a2b831a824a4dba
Andri Paulin <andpau200@gmail.com>
2021-12-03 00:13:02 +0100

    added category selection to shop

---
+++ ese2021-project-scaffolding/scaffolding/backend/src/controllers/order.controller.ts
@@ -0,0 +1,71 @@
+import express, { Router, Request, Response } from 'express';
+import { Order, OrderStatus } from '../models/order.model';
+
+const orderController: Router = express.Router();
+
+
+/**
+ * Create an order
+ */
+orderController.post('/', (req: Request, res: Response) => {
+    Order.create(req.body)
+      .then(create => {
+        res.status(201).send(create);
+      })
+      .catch(err => res.status(500).send(err));
+});
+
+/**
+ * read a specific order
+ */
+orderController.get('/:id', (req: Request, res: Response) => {
+    Order.findByPk(req.params.id).then(found => {
+      if (found != null) {
+        res.status(200).send(found);
+      } else {
+        res.sendStatus(404);
+      }
+    })
+      .catch(err => res.status(500).send(err));
+});
+
+/**
+ * read all orders
+ */
+orderController.get('/', (req: Request, res: Response) => {
+    Order.findAll()
+      .then(post => res.status(200).send(post))
+      .catch(err => res.status(500).send(err));
+});
+
+/**
+ * cancels an order
+ */
+orderController.put('/cancel/:id', (req: Request, res: Response) => {
+    Order.findByPk(req.params.id).then(found => {
+      if (found != null) {
+        found.update({'status': OrderStatus.Cancelled})
+        .then(updated => {res.status(200).send(updated); });
+      } else {
+        res.sendStatus(404);
+      }
+    })
+      .catch(err => res.status(500).send(err));
+});
+
+/**
+ *ships an order
+ */
+orderController.put('/ship/:id', (req: Request, res: Response) => {
+    Order.findByPk(req.params.id).then(found => {
+      if (found != null) {
+        found.update({'status': OrderStatus.Shipped})
+        .then(updated => {res.status(200).send(updated); });
+      } else {
+        res.sendStatus(404);
+      }
+    })
+      .catch(err => res.status(500).send(err));
+});
+
+export const OrderController: Router = orderController;
--- ese2021-project-scaffolding/scaffolding/backend/src/models/order.model.ts
+++ ese2021-project-scaffolding/scaffolding/backend/src/models/order.model.ts
@@ -1,89 +1,84 @@
-// import { Optional, Model, DataTypes, Sequelize } from 'sequelize';
+import { Optional, Model, DataTypes, Sequelize } from 'sequelize';
+import { Product } from '../models/product.model';
+import { User } from '../models/user.model';
 
-// import { OrderStatus, PaymentMethod } from '../constants';
 
-// /**
-//  * the order data of a user's purchase
-//  */
-// export interface OrderAttributes {
-//     orderId: number;
-//     purchaserId: number;
-//     product: number;
-//     status: OrderStatus;
-//     paymentMethod: PaymentMethod;
-//     /**
-//      * address information
-//      */
-//     firstName: string;
-//     lastName: string;
-//     street: string;
-//     city: string;
-//     plz: number;
-// }
-// export class Order extends Model<OrderAttributes> implements OrderAttributes {
-//     orderId: number;
-//     purchaserId: number;
-//     address: string;
-//     product: number;
-//     status: OrderStatus;
-//     paymentMethod: PaymentMethod;
+/**
+ * the order data of a user's purchase
+ */
+export interface OrderAttributes {
+  orderId: number;
+  purchaserId: number;
+  productId: number;
+  status: OrderStatus;
+  // paymentMethod: PaymentMethod;
+  /**
+   * address information
+   */
+  // firstName: string;
+  // lastName: string;
+  // street: string;
+  // city: string;
+  // plz: number;
+}
 
-//     /**
-//      * address information
-//      */
-//     firstName: string;
-//     lastName: string;
-//     street: string;
-//     city: string;
-//     plz: number;
-//     public static initialize(sequelize: Sequelize) {
-//         Order.init({
-//             orderId: {
-//                 type: DataTypes.INTEGER,
-//                 autoIncrement: true,
-//                 primaryKey: true
-//             },
-//             purchaserId: {
-//                 type: DataTypes.INTEGER,
-//                 allowNull: false,
-//             },
-//             status: {
-//                 type: DataTypes.STRING,
-//                 allowNull: false
-//             },
-//             paymentMethod: {
-//                 type: DataTypes.STRING,
-//                 allowNull: false
-//             },
-//             product: {
-//                 type: DataTypes.INTEGER,
-//                 allowNull: false
-//             },
-//             firstName: {
-//                 type: DataTypes.STRING,
-//                 allowNull: false
-//             },
-//             lastName: {
-//                 type: DataTypes.STRING,
-//                 allowNull: false
-//             },
-//             street: {
-//                 type: DataTypes.STRING,
-//                 allowNull: false
-//             },
-//             city: {
-//                 type: DataTypes.STRING,
-//                 allowNull: false
-//             },
-//             plz: {
-//                 type: DataTypes.INTEGER,
-//                 allowNull: false
-//             }
-//         },
-//             {
-//                 sequelize,
-//                 tableName: 'orders'
-//             }
-//         );
-//     }
-// }
+export interface OrderCreationAttributes extends Optional<OrderAttributes, 'orderId'> { }
+
+export class Order extends Model<OrderAttributes, OrderCreationAttributes> implements OrderAttributes {
+  orderId!: number;
+  purchaserId!: number;
+  productId!: number;
+  status!: OrderStatus;
+  // paymentMethod: PaymentMethod;
+
+  public static initialize(sequelize: Sequelize) {
+    Order.init({
+      orderId: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+      },
+      purchaserId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      status: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        defaultValue: OrderStatus.Pending
+      },
+      // paymentMethod: {
+      //     type: DataTypes.STRING,
+      //     allowNull: false
+      // },
+      productId: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+      },
+    },
+      {
+        sequelize,
+        tableName: 'orders'
+      }
+    );
+  }
+
+  public static createAssociations() {
+    Order.hasOne(Product, {
+      as: 'merchandise',
+      foreignKey: 'orderId'
+    });
+    Order.belongsTo(User, {
+      targetKey: 'userId',
+      as: 'buyer',
+      onDelete: 'cascade',
+      foreignKey: 'userId'
+    });
+  }
+}
+
+export const enum OrderStatus {
+  Pending = 'Pending',
+  Shipped = 'Shipped/Done',
+  Cancelled = 'Cancelled'
+}
