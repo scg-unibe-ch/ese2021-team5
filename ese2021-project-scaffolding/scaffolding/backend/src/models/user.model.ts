e97e15339047c5b89990b7a0f7d9b757350a832c
Alexander Aeschbacher <narwal27@protonmail.com>
2021-12-03 11:58:13 +0100

    Add default admin user
    Feel free to edit the admin fields in server.ts according to your needs.

--- ese2021-project-scaffolding/scaffolding/backend/src/models/user.model.ts
+++ ese2021-project-scaffolding/scaffolding/backend/src/models/user.model.ts
@@ -1,86 +1,94 @@
-import { TodoItem, TodoItemAttributes, TodoItemCreationAttributes } from './todoitem.model';
 import { Optional, Model, Sequelize, DataTypes } from 'sequelize';
 
 export interface UserAttributes {
-    userId: number;
-    userName: string;
-    password: string;
-    admin: boolean;
-    phoneNumber: number;
-    street: string;
-    city: string;
-    firstName: string;
-    lastName: string;
-    email: string;
-    plz: number;
-    birthday: Date;
+  userId: number;
+  userName: string;
+  password: string;
+  admin: boolean;
+  phoneNumber?: number;
+  street?: string;
+  city?: string;
+  firstName?: string;
+  lastName?: string;
+  email: string;
+  plz?: number;
+  birthday?: Date;
 }
 
 export interface UserCreationAttributes extends Optional<UserAttributes, 'userId'> { }
 
 export class User extends Model<UserAttributes, UserCreationAttributes> implements UserAttributes {
-    userId!: number;
-    userName!: string;
-    password!: string;
-    admin!: boolean;
-    phoneNumber: number;
-    street: string;
-    city: string;
-    firstName: string;
-    lastName: string;
-    email!: string;
-    plz: number;
-    birthday: Date;
+  userId!: number;
+  userName!: string;
+  password!: string;
+  admin!: boolean;
+  phoneNumber?: number;
+  street?: string;
+  city?: string;
+  firstName?: string;
+  lastName?: string;
+  email!: string;
+  plz?: number;
+  birthday?: Date;
 
-    public static initialize(sequelize: Sequelize) {
-        User.init({
-            userId: {
-                type: DataTypes.INTEGER,
-                autoIncrement: true,
-                primaryKey: true
-            },
-            userName: {
-                type: DataTypes.STRING,
-                allowNull: false,
-            },
-            password: {
-                type: DataTypes.STRING,
-                allowNull: false
-            },
-            admin: {
-                type: DataTypes.BOOLEAN,
-                defaultValue: false
-            },
-            street: {
-                type: DataTypes.STRING,
-            },
-            city: {
-                type: DataTypes.STRING,
-            },
-            firstName: {
-                type: DataTypes.STRING,
-            },
-            lastName: {
-                type: DataTypes.STRING,
-            },
-            email: {
-                type: DataTypes.STRING,
-                allowNull: false
-            },
-            plz: {
-                type: DataTypes.INTEGER,
-            },
-            phoneNumber: {
-                type: DataTypes.INTEGER
-            },
-            birthday: {
-                type: DataTypes.DATEONLY
-            },
-        },
-            {
-                sequelize,
-                tableName: 'users'
-            }
-        );
-    }
+  public static initialize(sequelize: Sequelize) {
+    User.init({
+      userId: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+      },
+      userName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      admin: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false
+      },
+      street: {
+        type: DataTypes.STRING,
+      },
+      city: {
+        type: DataTypes.STRING,
+      },
+      firstName: {
+        type: DataTypes.STRING,
+      },
+      lastName: {
+        type: DataTypes.STRING,
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      plz: {
+        type: DataTypes.INTEGER,
+      },
+      phoneNumber: {
+        type: DataTypes.INTEGER
+      },
+      birthday: {
+        type: DataTypes.DATEONLY
+      },
+    },
+      {
+        sequelize,
+        tableName: 'users'
+      }
+    );
+  }
+  public static makeAnAdmin() {
+    User.create({
+      'userName': 'admin',
+      'password': 'password',
+      'admin': true,
+      'email': 'admin@email.com'
+    });
+  }
 }
+
