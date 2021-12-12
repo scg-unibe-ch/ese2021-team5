aca824aa8e148853c7a142206f5617d14b2db844
Alexander Aeschbacher <narwal27@protonmail.com>
2021-12-12 11:50:56 +0100

    delete-product behaves differently
    Products are not deleted from the database anymore. instead they are they have a field
    called "active" that is turned to false.

--- ese2021-project-scaffolding/scaffolding/backend/src/controllers/product.controller.ts
+++ ese2021-project-scaffolding/scaffolding/backend/src/controllers/product.controller.ts
@@ -45,7 +45,7 @@ productController.delete('/:id', (req: Request, res: Response) => {
   Product.findByPk(req.params.id)
     .then(found => {
       if (found != null) {
-        found.destroy()
+        found.update({active: false})
           .then(item => res.status(200).send({ deleted: item }))
           .catch(err => res.status(500).send(err));
       } else {
--- ese2021-project-scaffolding/scaffolding/backend/src/models/product.model.ts
+++ ese2021-project-scaffolding/scaffolding/backend/src/models/product.model.ts
@@ -5,56 +5,63 @@ import { Optional, Model, DataTypes, Sequelize } from 'sequelize';
  * the store items sold
  */
 export interface ProductAttributes {
-    productId: number;
+  productId: number;
 
-    // in which category the item is
-    categoryId?: number;
-    title: string;
-    description?: string;
-    imageUri?: string;
-    price: number;
+  // in which category the item is
+  active: boolean;
+  categoryId?: number;
+  title: string;
+  description?: string;
+  imageUri?: string;
+  price: number;
 }
 
 
 export class Product extends Model<ProductAttributes> implements ProductAttributes {
-    productId: number;
+  productId: number;
 
-    // in which category the item is
-    categoryId: number;
-    title: string;
-    description: string;
-    imageUri: string;
-    price: number;
-    public static initialize(sequelize: Sequelize) {
-        Product.init({
-            productId: {
-                type: DataTypes.INTEGER,
-                autoIncrement: true,
-                primaryKey: true
-            },
-            categoryId: {
-                type: DataTypes.INTEGER,
-                defaultValue: null,
-            },
-            title: {
-                type: DataTypes.STRING,
-                allowNull: false
-            },
-            description: {
-                type: DataTypes.STRING
-            },
-            imageUri: {
-                type: DataTypes.STRING
-            },
-            price: {
-                type: DataTypes.INTEGER,
-                allowNull: false
-            }
-        },
-            {
-                sequelize,
-                tableName: 'products'
-            }
-        );
-    }
+  active: boolean;
+  // in which category the item is
+  categoryId: number;
+  title: string;
+  description: string;
+  imageUri: string;
+  price: number;
+  public static initialize(sequelize: Sequelize) {
+    Product.init({
+      productId: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+      },
+      categoryId: {
+        type: DataTypes.INTEGER,
+        defaultValue: null,
+      },
+      title: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      description: {
+        type: DataTypes.STRING
+      },
+      imageUri: {
+        type: DataTypes.STRING
+      },
+      price: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+      },
+      active: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: true
+
+      }
+    },
+      {
+        sequelize,
+        tableName: 'products'
+      }
+    );
+  }
 }
